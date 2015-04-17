cb26248ca983500a89bda63b820492346e095d39
jxnblk <jacksonblack@gmail.com>
2015-04-17 00:26:46 -0400

    Update bassdock and button link style

--- build.js
+++ build.js
@@ -16,7 +16,7 @@ data.src = path.join(__dirname, './templates');
 data.layout = fs.readFileSync(path.join(__dirname, './templates/layouts/base.html'), 'utf8');
 data.title = _.capitalize(data.name);
 data.baseurl = '//basscss.com';
-data.stylesheet = 'http://d2v52k3cl9vedd.cloudfront.net/bassdock/1.3.6/bassdock.min.css';
+data.stylesheet = 'http://d2v52k3cl9vedd.cloudfront.net/bassdock/1.3.7/bassdock.min.css';
 
 //data.asset_path = 'http://d2v52k3cl9vedd.cloudfront.net/basscss/';
 
