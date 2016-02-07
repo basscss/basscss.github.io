3e6c6729d0106d387688da9c9e1674e9a19e5bca
jxnblk <jacksonblack@gmail.com>
2016-02-07 12:48:20 -0500

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
 
 var webpack = require('webpack')
 var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
+var marked = require('marked')
+var markedExample = require('marked-example')
+var postcssImport = require('postcss-import')
+var postcssCustomProperties = require('postcss-custom-properties')
+var postcssColorFunction = require('postcss-color-function')
+
+var renderer = new marked.Renderer()
+
+renderer.code = markedExample({
+  classes: {
+    container: 'mb2 border border-thick border-darken rounded',
+    rendered: 'p2 border-bottom border-thick border-darken',
+    code: 'h5 m0 p2 border-none rounded-bottom',
+  }
+})
 
 module.exports = {
   entry: {
-    docs: './src/docs',
-    dev: './src/dev'
+    docs: [
+      './src/docs'
+    ],
+    dev: [
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+      './src/dev'
+    ]
   },
 
   output: {
@@ -19,21 +40,60 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loaders: [
+          'react-hot',
+          'babel'
+        ]
       },
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: 'json'
+      },
+      {
+        test: /\.md$/,
+        loaders: [
+          'html',
+          'markdown'
+        ]
       },
       {
         test: /\.css$/,
-        loader: 'css-loader'
+        loaders: [
+          'css',
+          'postcss'
+        ]
       }
     ]
   },
 
   plugins: [
-    new StaticSiteGeneratorPlugin('docs.bundle.js', ['/'], {})
-  ]
+    new StaticSiteGeneratorPlugin('docs.bundle.js', [
+      '/',
+      '/404.html'
+    ], {}),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  markdownLoader: { renderer },
+
+  postcss: function () {
+    return [
+      postcssImport(),
+      postcssCustomProperties(),
+      postcssColorFunction(),
+    ]
+  },
+
+  devServer: {
+    historyApiFallback: {
+      index: '/dev'
+    },
+    hot: true
+  },
+
+  node: {
+    fs: 'empty'
+  }
 }
 
